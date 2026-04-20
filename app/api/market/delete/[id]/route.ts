80ee592c64eac4a92aa36f3657152e7948163be7
nexora <nexoracard@gmail.com>
2026-04-20 17:15:53 +0700

    update dm layout 53

--- app/api/market/delete/[id]/route.ts
+++ app/api/market/delete/[id]/route.ts
@@ -3,10 +3,10 @@ import { prisma } from "@/lib/prisma";
 
 export async function DELETE(
   req: Request,
-  { params }: { params: { id: string } }
+  context: { params: Promise<{ id: string }> }
 ) {
   try {
-    const id = params.id;
+    const { id } = await context.params; // 🔥 ต้อง await
 
     await prisma.marketListing.delete({
       where: { id },
